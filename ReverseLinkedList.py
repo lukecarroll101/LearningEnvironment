93123ec003ae757fb8ce798f1b616c399f3083a3
Luke Carroll <luke_carroll98@outlook.com>
2024-10-10 16:50:58 +1100

    Fixed code bugs

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -4,12 +4,3 @@ import numpy
 arr = numpy.array([1, 2, 3, 4, 5])
 
 print(arr)
-
-
-
-# print("Hello World, this is Luke")
-
-# def main():
-#     hours, wage = 50, 70
-#     person = Employee()
-#     person. determine_weekly_salary(hours, wage)
